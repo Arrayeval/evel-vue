5e3fa4dc0855201dd4c676498a0a785d1fa55ce5
EvalGitHub <957010831@qq.com>
2019-05-19 16:45:33 +0800

    test

---
+++ test/index.js
@@ -0,0 +1,19 @@
+// import SelfVue from "evel-vue"
+import SelfVue from "../vu-coding/index.js"
+new SelfVue({
+  el: '#app',
+  data: {
+    title: 'hello world',
+    name: 'canfoo'
+  },
+  methods: {
+    clickMe: function () {
+      this.title = 'hello worldrr';
+    }
+  },
+  mounted: function () {
+    window.setTimeout(() => {
+      this.title = '你好';
+    }, 1000);
+  }
+});
